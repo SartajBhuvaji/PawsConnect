d3fb2704d978e4e2d35f350bbabe442b0dcab6d2
Sartaj Bhuvaji <s.bhuvaj@gmail.com>
2023-10-23 00:21:01 -0700

    updated right side on main page

--- client/src/components/home/Rightside.jsx
+++ client/src/components/home/Rightside.jsx
@@ -3,18 +3,18 @@ import styled from "styled-components";
 const Rightside = (props) => {
   return (
     <Container>
-      
       <FollowCard>
-        <Container>
-        <Title>
-        Latest Jobs
-        </Title>
-        </Container>
-
+        <Widget>
+          <a>
+            <div>
+              <span>Latest Jobs</span>
+            </div>
+          </a>
+        </Widget>
         <FeedList>
-       1
-       2
-       3
+       <li>1</li>
+       <li>2</li>
+       <li>3</li>
         </FeedList>
       </FollowCard>
     </Container>
@@ -39,26 +39,48 @@ const FollowCard = styled.div`
  
 `;
 
-const Title = styled.div`
-  background-color: #f3f2ef;
-  display: inline-flex;
-  align-items: center;
-  justify-content: space-between;
-  border: 1px solid rgba(0, 0, 0, 0.15);
-  border-radius: 10px;
-  font-size: 28px;
-  border-padding: 8px 16px 0px 16px;
-  margin-left: 16px;
-  width: 95%;
-  color: rgba(0, 0, 0, 0.6);
-  margin-top: 8px;
-  
+const Widget = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.15);
+  padding-top: 12px;
+  padding-bottom: 12px;
+  background-color: #0a66c2;
+
+  & > a {
+    text-decoration: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 4px 12px;
+
+    div {
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+      span {
+        font-size: 24px;
+        font-weight: bold;
+        line-height: 1.333;
+        &:first-child {
+          color: rgba(0, 0, 0, 0.6);
+        }
+        &:nth-child(2) {
+          color: rgba(0, 0, 0, 1);
+        }
+      }
+    }
+  }
+
+  svg {
+    color: rgba(0, 0, 0, 1);
+  }
 `;
 
 const FeedList = styled.ul`
   margin-top: 16px;
+  
   li {
     display: flex;
+    
     align-items: center;
     margin: 12px 0;
     position: relative;
